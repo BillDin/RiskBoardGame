acc5502a89fe484897d611f8213f8d900e11482c
John Owen <jco006@bucknell.edu>
2019-11-13 11:47:13 -0500

    Initial Commit

---
+++ src/main/Board.java
@@ -0,0 +1,23 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2019
+ * Instructor: Brian King
+ * Section: 11 AM
+ *
+ * Name: John Owen
+ * Date: 11/13
+ * Time: 11:45 AM
+ *
+ * Project: csci205_Final_Project
+ * Package: main
+ * Class: Board
+ *
+ * Description:
+ *
+ * *****************************************/
+
+
+package main;
+
+public class Board {
+}
